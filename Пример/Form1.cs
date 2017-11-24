b762c11bcd9a9d054b105967b333bc9e5b41aba8
AK <ankoz96@mail.ru>
2017-11-24 17:16:22 +0300

    Добавил проект

---
+++ Пример/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Пример
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double y = 1.1;
+            y = y - 1;
+            y = y / 2;
+            textBox1.Text = y.ToString();
+        }
+    }
+}
